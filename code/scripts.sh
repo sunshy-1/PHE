25bd0a8a939ce6175d7da350c72d9c5783dee701
sunshy-1 <open.code.github@gmail.com>
2026-04-10 15:22:58 +0800

    Update scripts.sh

--- code/scripts.sh
+++ code/scripts.sh
@@ -7,14 +7,14 @@ N_EPOCH_PRETRAIN=500
 N_EPOCH_FINETUNE=100
 CUDA=6
 
-# # ============ Pretrain ============
-# python pretrain_OAG_PHE.py \
-#     --data_dir "${DATA_DIR}/graph_CS.pk" \
-#     --pretrain_model_dir "${PRETRAIN_MODEL_DIR}" \
-#     --n_epoch ${N_EPOCH_PRETRAIN} \
-#     --cuda ${CUDA} \
-#     --batch_size 2048 \
-#     --conv_name 'hgt'
+# ============ Pretrain ============
+python pretrain_OAG_PHE.py \
+    --data_dir "${DATA_DIR}/graph_CS.pk" \
+    --pretrain_model_dir "${PRETRAIN_MODEL_DIR}" \
+    --n_epoch ${N_EPOCH_PRETRAIN} \
+    --cuda ${CUDA} \
+    --batch_size 2048 \
+    --conv_name 'hgt'
 
 # ============ Finetune ============
 DOMAINS=("_Engineering" "_CS" "_Materials")
